85358843fe091bc638b8b9df4ad4228350068b15
Maksym Rohachov <romax982@gmail.com>
2020-03-15 21:03:11 +0200

    Added implementation to retrieve individual user, modified properties file

--- src/main/java/com/web/app/rest/UserRegistrationRestController.java
+++ src/main/java/com/web/app/rest/UserRegistrationRestController.java
@@ -6,12 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
@@ -26,7 +25,22 @@ public class UserRegistrationRestController {
 
     @GetMapping("/")
     public ResponseEntity<List<UsersDTO>> listAllUsers() {
+        logger.info("Fetching all users");
         List<UsersDTO> users = userJpaRepository.findAll();
         return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UsersDTO> createUser(@RequestBody final UsersDTO user) {
+        logger.info("Creating User : {}", user);
+        userJpaRepository.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<UsersDTO>> getUserById(@PathVariable("id") final Long id) {
+        logger.info("Fetching User with id {}", id);
+        Optional<UsersDTO> user = userJpaRepository.findById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
