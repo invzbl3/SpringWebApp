89989b710e3f23f19ac80b43273ce0825366f5be
Maksym Rohachov <romax982@gmail.com>
2020-03-20 01:05:23 +0200

    Downgraded versions of bootstrap, angularjs dependencies for compatibility

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,17 +1,17 @@
 const app = angular.module('userregistrationsystem', ['ngRoute', 'ngResource']);
 
-app.component(function($routeProvider) {
+app.config(function ($routeProvider) {
     $routeProvider.when('/list-all-users', {
-        templateUrl : '/template/listuser.html',
-        controller : 'listUserController'
-    }).when('/register-new-user',{
-        templateUrl : '/template/userregistration.html',
-        controller : 'registerUserController'
-    }).when('/update-user/:id',{
-        templateUrl : '/template/userupdation.html' ,
-        controller : 'usersDetailsController'
+        templateUrl: '/template/listuser.html',
+        controller: 'listUserController'
+    }).when('/register-new-user', {
+        templateUrl: '/template/userregistration.html',
+        controller: 'registerUserController'
+    }).when('/update-user/:id', {
+        templateUrl: '/template/userupdation.html',
+        controller: 'usersDetailsController'
     }).otherwise({
-        redirectTo : '/home',
-        templateUrl : '/template/home.html',
+        redirectTo: '/home',
+        templateUrl: '/template/home.html',
     });
 });
