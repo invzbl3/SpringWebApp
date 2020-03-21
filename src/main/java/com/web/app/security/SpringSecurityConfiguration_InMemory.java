c175ef60c002274976133ea53299f6bfd01f7a61
Maksym Rohachov <romax982@gmail.com>
2020-03-21 19:22:05 +0200

    Alternative way how to fix the issue: There is no PasswordEncoder mapped for the id null

--- src/main/java/com/web/app/security/SpringSecurityConfiguration_InMemory.java
+++ src/main/java/com/web/app/security/SpringSecurityConfiguration_InMemory.java
@@ -1,6 +1,7 @@
 package com.web.app.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.
@@ -8,19 +9,33 @@ import org.springframework.security.config.annotation.authentication.builders.
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.
         WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SpringSecurityConfiguration_InMemory extends WebSecurityConfigurerAdapter {
-    @Autowired
+   /* @Autowired
     protected void configureGlobal(AuthenticationManagerBuilder auth)
             throws Exception {
         auth.inMemoryAuthentication().
-                withUser("user").password("{noop}password")
+                withUser("user").password("password")
                 .roles("USER");
         auth.
                 inMemoryAuthentication().withUser("admin").
-                password("{noop}password")
+                password("password")
                 .roles("USER", "ADMIN");
+    }*/
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(users.username("user").password("password").roles("USER").build());
+        manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN").build());
+        return manager;
+
     }
 
     @Override
